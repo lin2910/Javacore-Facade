507e8e8c49748960051d56c8cdcc3329467d69fd
Tokaeva A <64151867+lin2910@users.noreply.github.com>
2022-07-08 23:41:40 +0300

    Update BinOps.java

--- src/main/java/BinOps.java
+++ src/main/java/BinOps.java
@@ -11,14 +11,4 @@ public class BinOps {
                         * Integer.parseInt(b, 2));
     }
 
-    private static int intToBin(int str) {
-        int deg = 1;
-        int result = 0;
-        while (str > 0) {
-            result += str % 10 * deg;
-            deg *= 2;
-            str /= 10;
-        }
-        return result;
-    }
 }
